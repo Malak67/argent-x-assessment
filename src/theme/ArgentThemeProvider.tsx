5e49f21967d35a4baea1a1ed12e5e18c91f00c01
Florin Georgescu <flo_georgescu@hotmail.com>
2022-04-14 17:44:59 +0300

    first commit

---
+++ src/theme/ArgentThemeProvider.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react'
+import { ThemeProvider } from '@mui/material/styles'
+import createCache from '@emotion/cache'
+import { CacheProvider } from '@emotion/react'
+import { theme } from './theme'
+
+export const muiCache = createCache({
+  key: 'mui',
+  prepend: true
+})
+
+type ArgentThemeProps = {
+  children: React.ReactNode; // 👈️ type children
+};
+
+export const ArgentThemeProvider: FC<ArgentThemeProps> = (props: ArgentThemeProps) => {
+  return (
+    <CacheProvider value={muiCache}>
+      <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+    </CacheProvider>
+  )
+}
